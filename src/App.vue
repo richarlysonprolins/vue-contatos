c0f370ddb45266c32931eb3850196e02d510f181
richarlyson-best <richarlysongs@gmail.com>
2025-09-04 23:37:54 -0300

    feat: tela de login inicial

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+/* Se quiser fundo padrão para toda a aplicação */
+html, body, #app {
+  height: 100%;
+  margin: 0 50% 0 30%;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container fluid class="fill-height d-flex align-center justify-center">
+    <v-card width="400" class="pa-6">
+      <div class="d-flex flex-column align-center mb-4">
+        <img alt="Vue logo" class="logo" src="@/assets/NewLogoProlins.png" width="100%" height="100%" />
+        <h2 class="text-h6">Bem-vindo(a)</h2>
+      </div>
+
+      <v-form @submit.prevent="login">
+        <v-text-field
+          v-model="email"
+          label="E-mail"
+          type="email"
+          required
+        />
+        <v-text-field
+          v-model="password"
+          label="Senha"
+          type="password"
+          required
+        />
+        <v-btn type="submit" color="primary" block>Entrar</v-btn>
+      </v-form>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const email = ref('')
+const password = ref('')
+const router = useRouter()
+
+function login() {
+  if (email.value && password.value) {
+    router.push('/contatos')
+  } else {
+    alert('Preencha todos os campos!')
+  }
+}
+</script>
